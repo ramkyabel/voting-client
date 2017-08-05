5a099300ffd7e6a28a59b8c98af4cf08323dd549
ramky.gracia <ramky.gracia@gmail.com>
2017-08-05 03:10:17 -0400

    Added Voting Component

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
   entry: [
     'webpack-dev-server/client?http://localhost:8080',
     'webpack/hot/only-dev-server',
-    './src/index.js'
+    './src/index.jsx'
   ],
   module: {
     loaders: [{
@@ -14,7 +14,7 @@ module.exports = {
     }]
   },
   resolve: {
-    extensions: ['*', '.ts', '.tsx', '.js'] 
+    extensions: ['*', '.ts', '.tsx', '.js','.jsx'] 
   },
   output: {
     path: __dirname + '/dist',
